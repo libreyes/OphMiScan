bc0eb171a4788f25866590a3b393bda785d9ea5f
Mark Wadham <m@rkw.me>
2014-05-27 09:07:25 +0000

    updated to latest code

--- views/default/form_Element_OphMiScan_Document.php
+++ views/default/form_Element_OphMiScan_Document.php
@@ -17,14 +17,6 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
 ?>
-<section class="element <?php echo $element->elementType->class_name ?>"
-	data-element-type-id="<?php echo $element->elementType->id ?>"
-	data-element-type-class="<?php echo $element->elementType->class_name ?>"
-	data-element-type-name="<?php echo $element->elementType->name ?>"
-	data-element-display-order="<?php echo $element->elementType->display_order ?>">
-	<header class="element-header">
-		<h3 class="element-title">Document details</h3>
-	</header>
 	<div class="element-fields">
 		<?php echo $form->textField($element,'title')?>
 		<?php echo $form->textArea($element,'description',array('rows'=>8,'cols'=>60))?>
@@ -66,4 +58,3 @@
 			),
 		))?>
 	</div>
-</section>
